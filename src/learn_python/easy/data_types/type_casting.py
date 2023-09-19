3ac29872c8bfaccda5694a61e3421e707ff2bcc3
xransum <xransum@users.noreply.github.com>
2023-09-19 16:33:44 -0400

    Added type_casting.py

---
+++ src/learn_python/easy/data_types/type_casting.py
@@ -0,0 +1,24 @@
+"""Type casting to integer"""
+
+int(1) == 1
+int(2.8) == 2
+int('3') == 3
+
+"""Type casting to float"""
+
+float(1) == 1.0
+float(2.8) == 2.8
+float("3") == 3.0
+float("4.2") == 4.2
+
+"""Type casting to string"""
+
+str("s1") == 's1'
+str(2) == '2'
+str(3.0) == '3.0'
+
+"""Type casting to list"""
+
+list("abc") == ['a', 'b', 'c']
+str(['a', 'b', 'c']) == "['a', 'b', 'c']"
+
